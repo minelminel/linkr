207a18f0eb09f7c3cd8b7ee1a6d1181b9baf8bdc
Michael Lawrenson <ctrlcmdspace@gmail.com>
2019-06-22 15:36:51 -0400

    upgrade Django version

--- src/linkr/urls.py
+++ src/linkr/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 # from pages import views
-from pages.views import home_view
+from pages.views import home_view, about_view, contact_view
 
 urlpatterns = [
     path('',home_view, name='home'),
+    path('about/',about_view, name='about'),
+    path('contact/',contact_view, name='contact'),
     path('admin/', admin.site.urls),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request,*args,**krwags):
-    return render(request,"home.html",{})
+    return render(request, "home.html", {})
+
+def about_view(request,*args,**krwags):
+    my_context = {
+        "my_text": "this is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [123, 456, 789]
+    }
+    return render(request, "about.html", my_context)
+
+def contact_view(request,*args,**krwags):
+    return render(request, "contact.html", {})
